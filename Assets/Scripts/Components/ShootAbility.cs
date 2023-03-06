68ecf0791737f80d575f13e0e21ba259f9a6b2a1
Aleksandr <aleksandr.v.rybakov@gmail.com>
2023-03-06 22:53:18 +0300

    20230306_2250

--- Assets/Scripts/Components/ShootAbility.cs
+++ Assets/Scripts/Components/ShootAbility.cs
@@ -1,33 +1,49 @@
 ﻿using Components.Interfaces;
+using Unity.Entities;
 using UnityEngine;
 
 namespace Components
 {
     public class ShootAbility: MonoBehaviour, IShootAbility
     {
-        public GameObject _cannon;
+        public GameObject Cannon;
 
-        public GameObject _cannonCore;
+        public GameObject CannonCore;
         
         public GameObject TargetGameObject { get; set; }
-        
+
+        private EntityManager entityManager;
+        private Entity _cannonEntity;
+        private BlobAssetStore _blobAssetStore;
+
+        private void Awake()
+        {
+            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            _blobAssetStore = new BlobAssetStore();
+            GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, _blobAssetStore);
+            _cannonEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(Cannon, settings);
+        }
+
         public void Execute()
         {
             var targetCoordinates = TargetGameObject.transform.position;
             
-            Vector3 attackVector3 = targetCoordinates - _cannon.transform.position;
+            Vector3 attackVector3 = targetCoordinates - Cannon.transform.position;
             
-            var newBullet = Instantiate(_cannonCore, _cannon.transform.position, _cannon.transform.rotation);
-            
-            newBullet.transform.LookAt(targetCoordinates);
+            //var newBullet = Instantiate(Cannon, Cannon.transform.position, Cannon.transform.rotation);
 
-            newBullet.GetComponent<SphereCollider>().isTrigger = false;
+            Entity newEntity = entityManager.Instantiate(_cannonEntity);
 
-            var rigidbody = newBullet.GetComponent<Rigidbody>();
-            rigidbody.isKinematic = false;
-            rigidbody.AddForce(attackVector3 * 500f);
+
+            //newBullet.transform.LookAt(targetCoordinates);
+
+            //newBullet.GetComponent<SphereCollider>().isTrigger = false;
+
+            //var rigidbody = newBullet.GetComponent<Rigidbody>();
+            //rigidbody.isKinematic = false;
+            //rigidbody.AddForce(attackVector3 * 500f);
             
-            Destroy(newBullet.GetComponent<ShootAbility>());
+            //Destroy(newBullet.GetComponent<ShootAbility>());
         }
         
         
